8b4778b5233f222216d212b99c5c364fc9de9c8b
naijatechgirle <kellyeluagu@gmail.com>
2023-06-27 17:29:30 -0700

    exits the shell

--- builtin.c
+++ builtin.c
@@ -1,1 +1,88 @@
+#include"shell.h"
 
+/**
+* my_exit - exits the shell
+* @info: Structure containing potential arguments. Used to maintain
+*         constant function prototype.
+* Return: exits with a given exit status
+*         (0) if info.argv[0] != "exit"
+*/
+intmy_exit(info_t *info)
+{
+	int checkExit;
+
+		if (info->argv[1]) /* If there is an exit argument */
+		{
+			checkExit = my_erratoi(info->argv[1]);
+			if (checkExit == -1)
+			{
+				info->status = 2;
+				print_error(info, "Illegal number: ");
+				my_puts(info->argv[1]);
+				my_putchar('\n');
+				return (1);
+			}
+			info->err_num = my_erratoi(info->argv[1]);
+			return (-2);
+		}
+		info->err_num = -1;
+		return (-2);
+}
+
+/**
+* my_cd - changes the current directory of the process
+* @info: Structure containing potential arguments. Used to maintain
+*         constant function prototype.
+* Return: Always 0
+*/
+intmy_cd(info_t *info)
+{
+	char *s, *dir, buffer[1024];
+	int chdir_ret;
+
+	s = getcwd(buffer, 1024);
+	if (!s)
+		my_puts("TODO: >>getcwd failure emsg here<<\n");
+
+	if (!info->argv[1])
+	{
+		dir = my_getenv(info, "HOME=");
+		if (!dir)
+		{
+			chdir_ret = chdir((dir = my_getenv(info, "PWD=")) ? dir : "/");
+		}
+		else
+		{
+			chdir_ret = chdir(dir);
+		}
+	}
+	else if (my_strcmp(info->argv[1], "-") == 0)
+	{
+		if (!my_getenv(info, "OLDPWD="))
+		{
+			my_puts(s);
+			my_putchar('\n');
+			return (1);
+		}
+		my_puts(my_getenv(info, "OLDPWD="));
+		my_putchar('\n');
+		chdir_ret = chdir((dir = my_getenv(info, "OLDPWD=")) ? dir : "/");
+	}
+	else
+	{
+		chdir_ret = chdir(info->argv[1]);
+	}
+
+	if (chdir_ret == -1)
+	{
+		print_error(info, "can't cd to ");
+		my_puts(info->argv[1]);
+		my_putchar('\n');
+	}
+	else
+	{
+		my_setenv(info, "OLDPWD", my_getenv(info, "PWD="));
+		my_setenv(info, "PWD", getcwd(buffer, 1024));
+	}
+	return (0);
+}
